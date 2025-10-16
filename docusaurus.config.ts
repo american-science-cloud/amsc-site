93acf8511bb76a40fc095a4bca8c6bb148f6cfe0
Brian Gajus <gajusbd@ornl.gov>
2025-10-16 11:47:08 -0400

    Updates to config file and replace default social card with AmSC image

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'American Science Cloud',
+  tagline: 'Accelerating innovation with AI and advanced computing',
   favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -55,7 +55,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/amsc-social-card.jpg',
     colorMode: {
       defaultMode: 'light',
       disableSwitch: true,
