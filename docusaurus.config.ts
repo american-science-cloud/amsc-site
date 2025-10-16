280d63e9816b38578c0e0714b598359ccab8da9b
Brian Gajus <gajusbd@ornl.gov>
2025-10-16 10:06:29 -0400

    Fix Docusaurus baseUrl for GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://american-science-cloud.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/amsc-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'American Science Cloud', // Usually your GitHub org/user name.
+  organizationName: 'american-science-cloud', // Usually your GitHub org/user name.
   projectName: 'amsc-site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
